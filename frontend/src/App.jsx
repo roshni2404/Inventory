12c189d0c5a6c0bd11eb07cb244e1bf1cd830efc
Roshni Jain <roshni2404@users.noreply.github.com>
2025-07-29 19:03:36 +0530

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,8 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Root from './components/Root';
+import Root from './utils/Root';
 import Login from './pages/Login';
+import ProtectedRoutes from './utils/ProtectedRoutes';
 
 
 function App() {
@@ -9,9 +10,17 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Root />} />
-        <Route path="/admin/dashboard" element={<h1>admin dashboard</h1>} />
+        <Route path="/admin-dashboard" element={<ProtectedRoutes requiredRole={['admin']}>
+        <h1>admin dashboard</h1>
+        </ProtectedRoutes>
+        }  >
+        
+        
+        <Route index element={<h1>Summary of dashboard</h1>} />
+        </Route>
         <Route path="/customer/dashboard" element={<h1>customer dashboard</h1>} />
         <Route path="/login" element={<Login />} />
+        <Route path="/unauthorized" element={<p className='font-bold text-3xl mt-20 ml-20'>Unauthorized</p>} />
       </Routes>
     </Router>
 
---
+++ frontend/src/components/Sidebar.jsx
@@ -0,0 +1,9 @@
+import react from 'react';
+
+const Sidebar = () => {
+    return (
+        <div>Sidebar</div>
+    )
+}
+
+export default Sidebar;
