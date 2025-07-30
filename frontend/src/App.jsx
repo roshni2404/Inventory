938209bbecb0933c4155da72e537f78a54841038
Roshni Jain <roshni2404@users.noreply.github.com>
2025-07-30 18:22:20 +0530

    app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Root from './utils/Root';
 import Login from './pages/Login';
 import ProtectedRoutes from './utils/ProtectedRoutes';
 import Dashboard from './pages/Dashboard';
+import Categories from './components/Categories';
 
 function App() {
   return (
@@ -21,7 +22,21 @@ function App() {
         >
           <Route index element={<h1>Summary of dashboard</h1>} />
 
-          <Route path='categories' element={<h1>Categories</h1>} />
+          <Route path='categories' element={<Categories />}/>
+
+          <Route path='products' element=
+            {<h1>Products</h1>} />
+
+          <Route path='suppliers' element=
+            {<h1>Suppliers</h1>} />
+
+          <Route path='orders' element=
+            {<h1>Orders</h1>} />
+
+          <Route path='users' element=
+            {<h1>Users</h1>} />
+
+
         </Route>
 
         <Route path="/customer/dashboard" element={<h1>customer dashboard</h1>} />
---
+++ frontend/src/components/Categories.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+
+const Categories = () => {
+    const [categoryName, setCategoryName] = useState("");
+    const [categoryDescription, setCategoryDescription] = useState("");
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const response = await axios.post("http://localhost:3000/api/category/add", {categoryName, categoryDescription},
+            {
+                headers: { 
+                    Authorization: `Bearer ${localStorage.getItem('pos-token')}`,
+            },
+        }
+
+         );
+    
+       
+        if (response.data.success) {
+            alert("Category added successfully!");
+            setCategoryName("");
+            setCategoryDescription("");
+        } else {
+            console.error("Error adding category:", data);
+            alert("Error adding category. Please try again."); 
+        }
+    };
+
+    return (
+        <div className='p-4'>
+
+            <h1 className='text-2xl font-bold mb-8'>Category Management</h1>
+
+            <div className='flex flex-col lg:flex-row gap-4'></div>
+
+            <div className='lg:w-1/3'> 
+                <div className='bg-white p-4 shadow-md rounded-lg p-4'>
+                    <h2 className='text-center text-xl font-bold mb-4'>Add Category</h2>
+                    <form className='space-y-4' onSubmit={handleSubmit}>
+                        <div>
+                            <input type="text" placeholder="Category Name" className='border w-full p-2 rounded-md' onChange={(e) => setCategoryName(e.target.value)}/>
+                        </div>
+                        <div>
+                            <input type="text" placeholder="Category Description" className="border w-full p-2 rounded-md" onChange={(e) => setCategoryDescription(e.target.value)} />
+                        </div>
+                        <button type="submit" className='w-full rounded-md bg-green-500 text-white p-3 cursor-pointer'>Add Category</button>
+                    </form>
+
+                </div>
+
+                <div>
+
+                </div>
+            </div>
+
+
+
+        </div>
+
+    )
+}
+export default Categories;
